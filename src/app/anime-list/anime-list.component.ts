74c98fd43f8ae707268a4238d2a5dad039583aa0
Andres Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2025-01-12 15:00:20 -0500

    Create anime list container

---
+++ src/app/anime-list/anime-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-anime-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './anime-list.component.html',
+})
+export class AnimeListComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { AnimeListComponent } from './anime-list/anime-list.component';
+
+export const routes: Routes = [
+  { path: '', component: AnimeListComponent },
+];
